cd3410f878be6dcb8e64471cb5255d79a7e50eba
AleksandrStrulkov <astrulkov@yandex.ru>
2023-08-27 18:51:45 +0300

    Add function and test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,23 +1,63 @@
-import unittest
+import pytest
 from utils import arrs, dicts
 
 
-class TestArrs(unittest.TestCase):
+def test_get(fixture_get_list):
+    assert arrs.get(fixture_get_list, 0) == 1
+    assert arrs.get(fixture_get_list, 1) == 2
+    assert arrs.get(fixture_get_list, 2) == 3
+    assert arrs.get(fixture_get_list, 1, "test") == 2
+    assert arrs.get(fixture_get_list, -1, default=0) == 0
 
-    def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], -1, "test"), "test")
 
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
-        self.assertEqual(arrs.my_slice([], 0), [])
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], - 3, 4), [2, 3, 4])
-        self.assertEqual(arrs.my_slice([1, 2, 3], -4), [1, 2, 3])
+def test_my_slice(fixture_my_slice, fixture_my_slice_none):
+    assert arrs.my_slice(fixture_my_slice, 1, 3) == [2, 3]
+    assert arrs.my_slice(fixture_my_slice, 1) == [2, 3, 4]
+    assert arrs.my_slice(fixture_my_slice_none, 0) == []
+    assert arrs.my_slice(fixture_my_slice, - 3, 4) == [2, 3, 4]
+    assert arrs.my_slice(fixture_my_slice, - 5) == [1, 2, 3, 4]
 
-    def test_get_val(self):
-        self.assertEqual(dicts.get_val({"pk": 1}, "pk"), 1)
-        self.assertEqual(dicts.get_val({"pk": 1}, "pk", "git"), 1)
-        self.assertEqual(dicts.get_val({}, "pk", "git"), "git")
-        self.assertEqual(dicts.get_val({}, "pk", "bazaar"), "bazaar")
+
+def test_get_val(fixture_get_val, fixture_get_val_def):
+    assert dicts.get_val(fixture_get_val, "pk") == 1
+    assert dicts.get_val(fixture_get_val, "pk", "git") == 1
+    assert dicts.get_val(fixture_get_val_def, "pk", "git") == "git"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class TestArrs(unittest.TestCase):
+#
+#     def test_get(self):
+#         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+#         self.assertEqual(arrs.get([], -1, "test"), "test")
+#
+#     def test_slice(self):
+#         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+#         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+#         self.assertEqual(arrs.my_slice([], 0), [])
+#         self.assertEqual(arrs.my_slice([1, 2, 3, 4], - 3, 4), [2, 3, 4])
+#         self.assertEqual(arrs.my_slice([1, 2, 3], -4), [1, 2, 3])
+#
+#     def test_get_val(self):
+#         self.assertEqual(dicts.get_val({"pk": 1}, "pk"), 1)
+#         self.assertEqual(dicts.get_val({"pk": 1}, "pk", "git"), 1)
+#         self.assertEqual(dicts.get_val({}, "pk", "git"), "git")
+#         self.assertEqual(dicts.get_val({}, "pk", "bazaar"), "bazaar")
 
